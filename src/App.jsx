513efd6b8a971be41fc5828c9e0a71205b7a8d71
Aviv <aviv.dror@gmail.com>
2022-12-02 03:27:28 +0100

    api setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { get } from "./api/get";
 import { NavBar, Intro, Hero, Contact, Footer } from "./components/index";
 
-const App = () => (
-  <div className="wrapper">
-    <div className="gaunt">brother</div>
-    <div className="font">based</div>
-    <Intro />
-    <NavBar />
-    <Hero />
-    <Contact />
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [works, setWorks] = useState([]);
+  const [poems, setPoems] = useState([]);
+
+  const worksInit = async () => {
+    const workPromise = await get("/api/works?populate=media");
+    setWorks(workPromise);
+  };
+
+  const poemsInit = async () => {
+    const workPromise = await get("/api/poems");
+    setPoems(workPromise);
+  };
+
+  useEffect(() => {
+    worksInit();
+    poemsInit();
+  }, []);
+
+  const firstPoem = [];
+
+  if (works.length !== 0) {
+    console.log("fetch works", works.data);
+    console.log("fetch poems", poems.data[0].attributes.body);
+    firstPoem.push(String(poems.data[0].attributes.body));
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="gaunt">brother</div>
+      <div className="font">based</div>
+      {works.length !== 0 ? <p>{firstPoem}</p> : <></>}
+      <p>
+        normal <b>bold</b> <strong>strong</strong> <i>italic</i>
+        <b>
+          <i>bold italic</i>
+        </b>
+        <em>emphasize</em> <mark>mark</mark> <small>small</small>
+        <ins>inserted</ins> <br></br>
+        <sub>subscript</sub> <sup>super</sup> <big>big</big>
+      </p>
+      <Intro />
+      <NavBar />
+      <Hero />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,19 @@
+export const ORIGIN = "https://portfolio-back-production-ea92.up.railway.app";
+
+/*
+It comes
+Unadorned
+Like a phrase
+Strong enough to cast a spell;
+It comes
+Unbidden,
+Like the turn of sun through hills
+Or stars in wheels of song.
+The jeweled feet of women dance the earth.
+Arousing it to spring.
+Shoulders broad as a road bend to share the weight of years.
+Profiles breach the distance and lean
+Toward an ordinary kiss.
+Bliss.
+It comes naked into the world like a charm.
+*/
