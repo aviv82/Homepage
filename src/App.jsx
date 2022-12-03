5437f27c6354a13762ece59aa96059cc14f6a23a
Aviv <aviv.dror@gmail.com>
2022-12-03 22:22:55 +0100

    add lines

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   };
 
   const poemsInit = async () => {
-    const workPromise = await get("/api/poems");
+    const workPromise = await get("/api/poems?populate=line");
     setPoems(workPromise);
   };
 
@@ -21,35 +21,69 @@ const App = () => {
     poemsInit();
   }, []);
 
-  const firstPoem = [];
-
-  if (works.length !== 0) {
-    console.log("fetch works", works.data);
-    console.log("fetch poems", poems.data[0].attributes.body);
-    firstPoem.push(String(poems.data[0].attributes.body));
-  }
+  poems.length != 0
+    ? console.log("poems", poems.data[0].attributes.title)
+    : null;
 
   return (
     <div className="wrapper">
-      <div className="gaunt">brother</div>
-      <div className="font">based</div>
-      {works.length !== 0 ? <p>{firstPoem}</p> : <></>}
-      <p>
-        normal <b>bold</b> <strong>strong</strong> <i>italic</i>
-        <b>
-          <i>bold italic</i>
-        </b>
-        <em>emphasize</em> <mark>mark</mark> <small>small</small>
-        <ins>inserted</ins> <br></br>
-        <sub>subscript</sub> <sup>super</sup> <big>big</big>
-      </p>
-      <Intro />
+      {poems.length !== 0 ? <Intro poem={poems.data[0]} /> : <></>}
       <NavBar />
       <Hero />
       <Contact />
       <Footer />
+      <div className="font">based</div>
+      <div className="gaunt">brother</div>
     </div>
   );
 };
 
 export default App;
+
+/*
+      <div className="text-test">
+        <p>
+          normal <b>bold</b> <strong>strong</strong> <i>italic</i>
+          <b>
+            <i>bold italic</i>
+          </b>
+          <em>emphasize</em> <mark>mark</mark> <small>small</small>
+          <ins>inserted</ins> <br></br>
+          <sub>subscript</sub> <sup>super</sup> <big>big</big>
+        </p>
+      </div>
+      */
+
+/*
+  const firstPoem = [];
+  const parser = new DOMParser();
+  const parsedPoem = [];
+  const valueArray = [];
+
+  if (works.length !== 0) {
+    console.log("fetch works", works.data);
+    console.log("fetch poems", poems.data[0].attributes.body);
+    firstPoem.push(
+      parser.parseFromString(poems.data[0].attributes.body, "text/html")
+    );
+
+    for (let i = 0; i < firstPoem[0].body.childElementCount; i++) {
+      console.log("parsed", i, firstPoem[0].body.children[i]);
+    }
+
+    /*
+    firstPoem[0].body.children.map((line, i) =>
+      parsedPoem.push(<p key={i}>{line}</p>)
+    );
+    
+    for (let i = 0; i < firstPoem[0].body.children[0].length; i++) {
+      firstPoem.push(<p key={i}>{firstPoem[0].body.children[i]}</p>);
+    }
+    valueArray.push(firstPoem[0].children[0].children[1]);
+    console.log(
+      "html array:",
+      firstPoem[0].children[0].children[1],
+      valueArray[0]
+    );
+  }
+*/
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export const Intro = () => {
-  return <div>Intro</div>;
+export const Intro = ({ poem }) => {
+  return (
+    <div className="poem">
+      {poem.attributes.line.map((line, i) => (
+        <p dangerouslySetInnerHTML={{ __html: line.text }} key={i}></p>
+      ))}
+      <p key={poem.attributes.line.length + 2}>
+        <br></br>-
+        <i>
+          {" "}
+          <b>{poem.attributes.author},</b> {poem.attributes.title}
+        </i>
+      </p>
+    </div>
+  );
 };
