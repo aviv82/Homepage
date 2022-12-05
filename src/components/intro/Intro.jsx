11903cc8e8f827fa8f4a53b0c1bae8da302b3a07
Aviv <aviv.dror@gmail.com>
2022-12-05 23:53:28 +0100

    bg colour

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
 import { useRef } from "react";
 import { useState } from "react";
 
 export const Intro = ({ poem }) => {
   const [isScroll, setIsScroll] = useState(false);
 
-  // const scrollYValue = useRef(0);
+  /*
+  const locationRef = useRef();
+  const [isViewable, setIsViewable] = useState();
+
+  console.log("is it visible?", isViewable);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((values) => {
+      const value = values[0];
+      setIsViewable(value.isIntersecting);
+    });
+    observer.observe(locationRef.current);
+  }, []);
+  */
 
   const handlePoemScroll = () => {
     setIsScroll(window.scrollY);
@@ -19,17 +33,25 @@ export const Intro = ({ poem }) => {
     <div className="poem">
       {poem.attributes.line.map((line, i) => (
         <p
-          className={isScroll > i * 20 ? "show" : "hide"}
+          className={isScroll >= i * 20 ? "show" : "hide"}
           dangerouslySetInnerHTML={{ __html: line.text }}
           key={i}
         ></p>
       ))}
-      <p key={poem.attributes.line.length + 2}>
-        <br></br>-
-        <i>
-          <b>{poem.attributes.author},</b> {poem.attributes.title}
-        </i>
-      </p>
+      <div className="title">
+        <p
+          className={
+            isScroll >= (poem.attributes.line.length + 2) * 20 ? "show" : "hide"
+          }
+          // ref={locationRef}
+          key={poem.attributes.line.length + 2}
+        >
+          <br></br>-
+          <i>
+            <b>{poem.attributes.author},</b> {poem.attributes.title}
+          </i>
+        </p>
+      </div>
     </div>
   );
 };
